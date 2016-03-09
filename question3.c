498e27f062acbfc1f43265579fc639117c43c4bb
zhangjunqi13 <zhangjunqi13@mails.tsinghua.edu.cn>
2016-03-09 19:30:28 +0800

    Update question3.c

--- question3.c
+++ question3.c
@@ -2,29 +2,29 @@
 #include <sstream>
 #include <iomanip
 
-#define NUM 128
-#define CON 32
-#define BEGIN 17
+#define NUM 128   //页数目
+#define CON 32   //页大小（byte数目）
+#define BEGIN 17  //页目录表的物理地址
 
 using namespace std;
 
 
-short mem[NUM][CON]={{1,2,3},{4,5,6}};
+short mem[NUM][CON];   //物理内存
 
 
-void search(short VAddress){
-	int pde_index = (VAddress >> 10) & 0x1f;
+void search(short VAddress){   //VAddress为虚地址
+	int pde_index = (VAddress >> 10) & 0x1f;   
 	int pte_index = (VAddress >> 5) & 0x1f;
 
-	short page_index = mem[BEGIN][pde_index];
-	short thing = mem[page_index][pte_index];
+	short page_index = mem[BEGIN][pde_index];    //页表所对应的页号
+	short thing = mem[page_index][pte_index];    //物理地址所对应的内容
 
 	cout<<thing<<endl;
 }
 
 int main(){
 	search(6);
-	//search(45621);
+	search(45621);
 
 	return 0;
 }
